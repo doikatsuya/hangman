58b58ff680b64162659b442e9393aab981ef310c
土居 勝也 <rsj09118@doi-katsuya-nokonpyuta-2.local>
2019-05-01 01:16:54 +0900

    another update

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 "use strict";
 
 // console.log("Hello World from main.js!");
-console.log("It's me!")
-console.log('It\'s me too!');
+// console.log("It's me!")
+// console.log('It\'s me too!');
+//
+// console.log('Hello\n wor\tld!');
+// console.log("Hello"+" everyone");
+console.log(10+3);
+console.log(10-3);
+console.log(10*3);
+console.log(10/3);
+console.log(10%3);
+console.log(10**3);
 
-console.log('Hello\n wor\tld!');
-console.log("Hello"+" everyone");
+console.log(2+10*3);
+console.log((2+10)*3);
