5d67f46f4564b5251e9c58cf2d8ef660d579ddc8
doikatsuya <doikatsuya0421@gmail.com>
2019-05-01 14:51:20 +0900

    another update

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 "use strict";
-
-// console.log("Hello World from main.js!");
-// console.log("It's me!")
-// console.log('It\'s me too!');
-//
-// console.log('Hello\n wor\tld!');
-// console.log("Hello"+" everyone");
-console.log(10+3);
-console.log(10-3);
-console.log(10*3);
-console.log(10/3);
-console.log(10%3);
-console.log(10**3);
-
-console.log(2+10*3);
-console.log((2+10)*3);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(parseInt("5",10) + 3);
+// score >=80 ? console.log("Great") : console.log("OK...!");
+// const score = 60;
+// const name = "taguchi";
+// if(score>=50 && name==="taguchi"){
+//   console.log("Great! Taguchi");
+// }
+// if(signal === "red"){
+//   console.log("stop!");
+// }else if(signal ==="yellow"){
+//   console.log("caution!");
+// }else if(signal ==="blue"){
+//   console.log("go!");
+// }
+let signal= "blue"
+switch (signal){
+  case "red":
+    console.log("stop case");
+    break;
+  case "yellow":
+    console.log("caution case");
+    break;
+  case "blue":
+    console.log("go case");
+    break;
+  default:
+    console.log("What is "+signal);
+    break;
+}
