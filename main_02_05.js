3aa4498c15267235c03da346e2e711d9a72c0708
doikatsuya <doikatsuya0421@gmail.com>
2019-05-03 02:06:22 +0900

    js need to update

---
+++ main_02_05.js
@@ -0,0 +1,26 @@
+"use strict";
+{
+  // const str = "hello";
+  // // console.log(str.length);
+  // console.log(str.substring(2,4));
+  // console.log(str[1]);
+
+  //Math
+  // console.log(Math.PI);
+  //
+  // console.log(Math.random());//0...1
+  // console.log(Math.floor(Math.random()*6+1));
+  // console.log(Math.abs(-5));
+
+  //Date
+  const d = new Date();
+  // console.log(d.getFullYear());
+  // console.log(d.getMonth()+1);
+  // console.log(d.getDate());
+  // console.log(d.getHours());
+  // console.log(d.getMinutes());
+  // console.log(d.getSeconds());
+  // console.log(d.getMilliseconds());
+  console.log(d.getTime());
+
+}
